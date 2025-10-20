a720bcecde346419511df948f5018503eb32bc3e
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-10-20 13:26:01 +0530

    Time: 5 ms (78.79%), Space: 62.9 MB (93.89%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -2,14 +2,22 @@
  * @param {number} x
  * @return {boolean}
  */
+
+
 var isPalindrome = function(x) {
-  if (x < 0) return false;
-  let xCopy = x;
-  let rev = 0;
-  while (x > 0) {
-    let rem = x % 10;
-    rev = rev * 10 + rem;
-    x = Math.floor(x / 10);
-  }
-  return rev === xCopy;
+    if (x < 0) return false;
+    let xcopy = x;
+    let rev=0;
+
+
+    while (x > 0) {
+        let rem = x % 10;
+        rev = (10 * rev) + rem;
+        x = Math.floor(x / 10);
+    }
+    if (rev == xcopy) {
+        return true;
+    } else {
+        return false;
+    }
 };
