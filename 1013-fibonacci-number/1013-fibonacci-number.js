d446aa95f366b408b14f8ddb783df15ab7483426
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-11-04 21:12:41 +0530

    Time: 55 ms (53.87%), Space: 54.8 MB (11.3%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.js
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -2,13 +2,13 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function (n) {
-    if (n < 2) return n; 
-    let a = 0, b = 1;
-    for (let i = 2; i <= n; i++) {
-        let temp = a + b;
-        a = b;
-        b = temp;
-    }
-    return b;
+var fib = function(n) {
+  if (n < 2) return n;          
+  let a = 0, b = 1;
+  for (let i = 2; i <= n; i++) {
+    let temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
 };
