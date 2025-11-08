fe4af46e8ee09641a1b507f70a637132d40280e4
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-11-08 13:09:46 +0530

    Time: 12 ms (28.67%), Space: 57 MB (53.44%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -2,11 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
-  var singleNumber = function(nums) {
-      let xor = 0;
-      for (let i = 0; i < nums.length; i++) {
-          xor = xor ^ nums[i];
-      }
-      return xor;
+ var singleNumber = function(nums) {
+      let hash = {};
+
+    for( let i=0; i<nums.length; i++){
+        if(!hash[nums[i]]){
+            hash[nums[i]]=1;
+        }else{
+            hash[nums[i]]++;
+            }
+    }
+    for (let i=0; i<nums.length; i++){
+        if(hash[nums[i]]==1){
+            return nums[i];
+        }
+    }
+
   };
-    
+
+let nums = [4, 1, 2, 1, 2];
+let result = singleNumber(nums);
+console.log(result);
