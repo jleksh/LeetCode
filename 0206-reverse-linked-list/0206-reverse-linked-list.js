1a5996bca6d96a16a1b6450ec173eaa74f38eac6
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-11-20 19:49:16 +0530

    Time: 1 ms (11.31%), Space: 57.5 MB (43.46%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -10,10 +10,14 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    if (head === null || head.next === null) return head;
-
-    let newHead = reverseList(head.next);
-    head.next.next = head;
-    head.next = null;
-    return newHead;
+    let prev= null;
+    let curr= head;
+    while(curr){
+        let temp=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=temp;
+    }
+    head=prev;
+    return head;
 }
