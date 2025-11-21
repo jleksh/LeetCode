5223f6faedd5178b58b4cf24202394bf88b622d5
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-11-21 08:39:14 +0530

    Time: 55 ms (68.73%), Space: 60.3 MB (6.05%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -12,13 +12,14 @@
  */
 var hasCycle = function(head) {
     let seenNodes = new Set();
-    let curr = head;
-    while (curr !== null) {
-        if (seenNodes.has(curr)) {
+    let curr= head;
+
+    while(curr!= null) {
+        if(seenNodes.has(curr)){
             return true;
         }
         seenNodes.add(curr);
-        curr = curr.next;
+        curr=curr.next;
     }
     return false;
 };
