f5ec10d0c05d0455f2628bf592aa0036a31e2c5f
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-11-21 18:22:45 +0530

    Time: 14 ms (34.11%), Space: 77.8 MB (56.55%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -9,19 +9,33 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+var isPalindrome = function(head) {
 
- var isPalindrome = function(head) {
-    let arr = [];
-    let curr = head;
-    while (curr !== null) {
-        arr.push(curr.val);
-        curr = curr.next;
+    // find the mid element
+    let slow=fast=head;
+    while (fast && fast.next){
+        slow=slow.next;
+        fast=fast.next.next;
     }
-    let left = 0, right = arr.length - 1;
-    while (left < right) {
-        if (arr[left++] !== arr[right--]) return false;
+    //reverse the list
+    let prev= null; 
+    let curr=slow;
+    while(curr){
+        let temp=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=temp;
     }
+    //palindrome or not
+    let firstList=head;
+    let secondList=prev;
+    while(secondList){
+        if(firstList.val !==secondList.val){
+            return false;
+            }
+            firstList  = firstList.next;
+            secondList = secondList.next;
+        }
     return true;
 };
-
     
