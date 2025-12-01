e20429f2f15f6953c0d1c233c23e00690df6fe05
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-12-01 10:46:24 +0530

    Time: 0 ms (100%), Space: 53.9 MB (65.64%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,17 +3,8 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let i = s.length - 1;
-    let length = 0;
-
-    while (i >= 0 && s[i] === " ") {
-        i--;
-    }
-
-    while (i >= 0 && s[i] !== " ") {
-        length++;
-        i--;
-    }
-
-    return length;
-}
+    s= s.trim();
+    s= s.split(" ");
+    return s[s.length -1].length;
+    
+};
