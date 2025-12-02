4d532581314d7146b0edb644945e9d8a708ecc1d
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-12-02 19:33:57 +0530

    Time: 1 ms (83.5%), Space: 58.2 MB (53.68%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.js
+++ 2942-find-words-containing-character/2942-find-words-containing-character.js
@@ -4,13 +4,13 @@
  * @return {number[]}
  */
 var findWordsContaining = function(words, x) {
-    //add empty result array
     let res= [];
-   // iterate through array& check if the target element
-   //if present, return index to res array
     for(let i=0; i<words.length;i++){
-        if(words[i].includes(x)){
-            res.push(i);
+        for(let j=0; j<words[i].length;j++){
+            if(words[i][j]===x){
+                res.push(i);
+                break;
+            }
         }
     }
     return res;
