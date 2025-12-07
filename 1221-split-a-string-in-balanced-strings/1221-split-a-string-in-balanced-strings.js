373771a5b6cd959ef49ce9684c1cb819e97f2cc9
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-12-07 17:38:13 +0530

    Time: 2 ms (9.82%), Space: 54.8 MB (25.72%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -3,15 +3,20 @@
  * @return {number}
  */
 var balancedStringSplit = function(s) {
-    let balance = 0;
-    let result = 0;
-
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === 'L') balance++;
-        else balance--;
-
-        if (balance === 0) result++;
+  let count=0;
+  let R=0;
+  let L=0;
+  for(let i=0;i<s.length;i++){
+    if(s[i]==="R"){
+        ++R;
+    }else{
+        ++L;
     }
-
-    return result;
+    if(L===R){
+        ++count;
+        L=0;
+        R=0;
+    }
+  }
+  return count;
 };
