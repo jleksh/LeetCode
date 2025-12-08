8b5a2da79467cef0d6dc78a2f20d3eb2e46c6bbc
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-12-08 19:48:06 +0530

    Time: 0 ms (100%), Space: 54.8 MB (55.57%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -2,13 +2,17 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-    if(!strs.length) return '';
-    
-    for(let i = 0; i < strs[0].length; i++) {
-        for(let s of strs) {
-            if(s[i] !== strs[0][i]) return s.slice(0, i);
-        }
-    }
-    return strs[0];
-};
+  var longestCommonPrefix = function(strs) {
+      let x = 0;
+      while (x < strs[0].length) {
+          let ch = strs[0][x];
+          for (let i = 1; i < strs.length; i++) {
+              if (ch != strs[i][x] || x == strs[i].length) {
+                  return strs[0].substring(0, x);
+              }
+          }
+          ++x;
+      }
+      return strs[0];
+  };
+     
