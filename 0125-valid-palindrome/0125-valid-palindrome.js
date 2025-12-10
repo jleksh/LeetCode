e482da512c7ed5e2056fa2ed69108acb18f52bfe
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-12-10 18:39:44 +0530

    Time: 7 ms (35.28%), Space: 60.4 MB (27.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,14 +4,21 @@
  */
 var isPalindrome = function (s) {
     s = s.toLowerCase();
-    let filteredString = "";
-    let rev = "";
-    for (let i = 0; i < s.length; i++) {
-        if (s[i].match(/[a-z0-9]/i)){
-            filteredString = filteredString + s[i];
+    let i=0;
+    let j=s.length-1;
+    while(i<j){
+        if(!s[i].match(/[a-z0-9]/i)){
+            ++i;
+        }else if(!s[j].match(/[a-z0-9]/i)){
+            --j;
+        }else if(s[i]===s[j]){
+            ++i;
+            --j;
+        }else{
+            return false;
         }
     }
-    rev = filteredString.split("").reverse().join("");
-    return filteredString === rev;
+    return true;
+
 };
      
