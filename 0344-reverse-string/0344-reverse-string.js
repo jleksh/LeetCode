25a56a3403ae112ee1e7872119cb3cd5d1118568
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-12-10 08:22:18 +0530

    Time: 2 ms (26.32%), Space: 60.3 MB (73.15%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,14 +3,13 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
- let len=s.length;
- let halfLen=Math.floor(len/2);
-
- for(let i=0; i<halfLen;i++){
-    //swapping s[i],s[len-1-i]
-    let temp=s[i];
-    s[i]=s[len-1-i];
-    s[len-1-i]=temp;
- }
+    let n= s.length;
+    let mid= Math.floor(n/2);
+    for(let i=0; i<mid; i++){
+        let temp=s[i];
+        s[i]=s[n-1-i];
+        s[n-1-i]=temp;
+    }
+    return s.join("");
     
 };
