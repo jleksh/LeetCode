c8f75d727b604a739d11114c9780795b26b60736
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-12-11 11:51:15 +0530

    Time: 0 ms (100%), Space: 60.1 MB (16.5%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
@@ -2,14 +2,14 @@
  * @param {string} num
  * @return {string}
  */
-  var largestOddNumber = function(s) {
-      let n = s.length - 1;
-      while (n >= 0) {
-          if (Number(s[n]) % 2 == 1) {
-              return s.substring(0, n + 1);
-          }
-          --n;
-      }
-      return "";
+  var largestOddNumber = function(num) {
+    let n=num.length-1;
+    while(n>=0){
+        if(Number(num[n])%2 === 1){
+            return num.substring(0,n+1);
+        }
+        --n;
+    }
+    return "";
   };
       
