87aed8c8df518e56d420899d8c80b726bd490676
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-12-19 06:26:55 +0530

    Time: 0 ms (100%), Space: 54 MB (57.97%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.js
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.js
@@ -1,7 +1,6 @@
 
 var MyStack = function() {
-  this.q1 = [];
-  this.q2 = [];
+  this.q = [];
 };
 
 /** 
@@ -9,21 +8,18 @@ var MyStack = function() {
  * @return {void}
  */
 MyStack.prototype.push = function(x) {
-  this.q1.push(x);
+  this.q.push(x);
 };
 
 /**
  * @return {number}
  */
 MyStack.prototype.pop = function() {
-  let n = this.q1.length;
+  let n = this.q.length;
   for (let i = 0; i < n - 1; i++) {
-    this.q2.push(this.q1.shift());
+    this.q.push(this.q.shift());
   }
-  let ans = this.q1.shift();
-  let temp = this.q1;
-  this.q1 = this.q2;
-  this.q2 = temp;
+  let ans = this.q.shift();
   return ans;
 };
 
@@ -33,15 +29,12 @@ MyStack.prototype.pop = function() {
  */
 
 MyStack.prototype.top = function() {
-  let n = this.q1.length;
+  let n = this.q.length;
   for (let i = 0; i < n - 1; i++) {
-    this.q2.push(this.q1.shift());
+    this.q.push(this.q.shift());
   }
-  let front = this.q1[0];
-  this.q2.push(this.q1.shift());
-  let temp = this.q1;
-  this.q1 = this.q2;
-  this.q2 = temp;
+  let front = this.q[0];
+  this.q.push(this.q.shift());
   return front;
 };
 
@@ -49,7 +42,7 @@ MyStack.prototype.top = function() {
  * @return {boolean}
  */
 MyStack.prototype.empty = function() {
-  return this.q1.length === 0;
+  return this.q.length === 0;
 };
 
 /** 
