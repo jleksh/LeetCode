c8f368c501cacdc97de2a25bfdf96026c6c2db99
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-12-23 13:13:01 +0530

    Time: 20 ms (18.3%), Space: 59.9 MB (19.97%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -2,23 +2,18 @@
  * @param {string[]} tokens
  * @return {number}
  */
-var evalRPN = function(arr) {
+var evalRPN = function(tokens) {
     let stack = [];
-    const map = {
-        "+": (a,b) => (b+a),
-        "*": (a,b) => (b*a),
-        "-": (a,b) => (b-a),
-        "/": (a,b) => Math.trunc(b/a),
-    };
-        for(let i=0; i < arr.length; i++){
-            if(map[arr[i]]) {
-                let a = stack.pop();
-                let b = stack.pop();
-                let ans = map[arr[i]](+a,+b);
-                stack.push(ans);
-            } else {
-                stack.push(arr[i]) 
+    for (let i=0; i<tokens.length;i++){
+        if(tokens[i]==='+'||tokens[i]==='-'||tokens[i]==='*'||tokens[i]==='/'){
+            let b=stack.pop();
+            let a=stack.pop();
+            let ans=eval(`${a} ${tokens[i]} ${b}`);
+            stack.push(Math.trunc(ans));
+            }else{
+                stack.push(tokens[i])
             }
         }
-    return Number(stack.pop());
+    return +stack.pop();
+
 };
