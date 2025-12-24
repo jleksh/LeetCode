2bc0f5260505ce6c78d82296d17aa5e069fef9ad
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-12-24 20:23:08 +0530

    Time: 1 ms (69.31%), Space: 55.5 MB (76.03%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -2,24 +2,19 @@
  * @param {number} x
  * @return {number}
  */
-  var mySqrt = function(x) {
-  var left = 1;
-  var right = Math.floor(x / 2) + 1;
-  var mid;
-
-  while (left <= right) {
-    mid = Math.floor((left + right) / 2);
-
-    if (mid * mid > x) {
-      right = mid - 1;
-    } else if (mid * mid < x) {
-      left = mid + 1;
-    } else {
-      return mid;
-    }
-  }
-
-  
-
-  return right;
-};
+var mySqrt = function(x) {
+      if (x < 2) return x;
+      let l = 2;
+      let r = Math.floor(x / 2);
+      while (l <= r) {
+          let m = Math.floor((l + r) / 2);
+          if (x === m * m) {
+              return m;
+          } else if (x < m * m) {
+              r = m - 1;
+          } else {
+              l = m + 1;
+          }
+      }
+      return r;
+  };
