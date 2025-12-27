fb31bb62dbd15da1a75a304eb843cf0b00c97747
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2025-12-27 19:23:54 +0530

    Time: 44 ms (59.1%), Space: 52.7 MB (96.89%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    return function(n) {
+        let l = 1;
+        let r = n;
+        while (l < r) {
+            let m = l + Math.floor((r - l) / 2);
+            if (!isBadVersion(m)) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return r;
+    };
+};
