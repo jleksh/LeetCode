f02847126be3c0e226bde5f75c14fd0800c02f2c
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2026-01-26 23:03:24 +0530

    Time: 51 ms (90.66%), Space: 70.2 MB (44.29%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @return {number[][]}
+ */
+var minimumAbsDifference = function(arr) {
+    arr.sort((a, b) => a - b);
+
+    let minDiff = Infinity;
+    let result = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        let diff = arr[i] - arr[i - 1];
+
+        if (diff < minDiff) {
+            minDiff = diff;
+            result = [[arr[i - 1], arr[i]]];
+        } else if (diff === minDiff) {
+            result.push([arr[i - 1], arr[i]]);
+        }
+    }
+
+    return result;
+};
