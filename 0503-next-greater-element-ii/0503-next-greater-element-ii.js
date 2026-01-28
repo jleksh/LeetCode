d2c91045c30b425b68e60bff5b1b6d9fa5bdd82f
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2026-01-28 21:35:54 +0530

    Time: 13 ms (23.2%), Space: 64.4 MB (13.81%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.js
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.js
@@ -2,22 +2,23 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var nextGreaterElements = function(arr) {
+var nextGreaterElements = function(nums) {
+    let arr=[...nums,...nums];
     let n = arr.length;
     let stack = [];
     let ans = Array(n).fill(-1);
     stack.push(arr[n-1]);
-    for(let i=(2*n)-2; i >= 0; i--){
+    for(let i=n-2; i >= 0; i--){
         while(stack.length){
             let top = stack[stack.length-1];
-            if(arr[i % n] < top){
-                ans[i % n] = top;
+            if(arr[i] < top){
+                ans[i] = top;
                 break;
             } else{
                 stack.pop();
             }
         }
-        stack.push(arr[i % n]);
+        stack.push(arr[i]);
     }
-    return ans.slice(0, n);
+    return ans.slice(0, n/2);
 };
