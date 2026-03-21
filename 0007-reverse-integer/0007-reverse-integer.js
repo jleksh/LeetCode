5d7a3ec59cf994210f8f6514a68ac793ed3c1be4
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2026-03-21 21:54:45 +0530

    Time: 43 ms (90.8%), Space: 57.2 MB (22.6%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -2,23 +2,17 @@
  * @param {number} x
  * @return {number}
  */
- 
-var reverse = function (x) {
-    let xCopy = x;
-    x = Math.abs(x);
-    let rev = 0;
+var reverse = function(x) {
+  let xCopy = x;
+  x = Math.abs(x);
+  let rev = 0;
+  while (x > 0) {
+    let last = x % 10;
+    rev = rev * 10 + last;
+    x = Math.floor(x / 10);
+  }
+  if (rev > 2**31 - 1) return 0;
+  return xCopy < 0 ? -rev : rev;
+};
 
-
-    while (x > 0) {
-        let rem = x % 10;
-        rev = (10 * rev) + rem;
-        x = Math.floor(x / 10);
-    }
-
-    let limit = Math.pow(2, 31);
-    if (rev < -limit || rev > limit) return 0; 
-
-    return (xCopy < 0) ? -rev : rev;
-}
-
-
+console.log(reverse(123));
