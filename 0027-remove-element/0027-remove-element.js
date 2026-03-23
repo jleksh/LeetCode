757713816c8813fd22a5cb2073b24ecb47b29bdb
Arya Lekshmi J <137407770+jleksh@users.noreply.github.com>
2026-03-23 22:20:49 +0530

    Time: 0 ms (100%), Space: 53.9 MB (51.55%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,14 +4,12 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    let k=0;
-    for(let i=0; i<nums.length; i++){
-        if(nums[i]!=val){
-            nums[k]=nums[i];
-            k++;
-        }
+  let x = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != val) {
+      nums[x] = nums[i];
+      x++;
     }
-    
-  return k;
-}
- 
+  }
+  return x;
+};
